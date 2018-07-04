6322118ca3ff04115f852064880797602b655018
Megan McKeough <megan.mckeough@gmail.com>
2018-07-04 11:35:51 +1000

    image function added

--- app.js
+++ app.js
@@ -19,17 +19,22 @@ var square9 = document.querySelector('.nine');
 //targeting square images
 var square1ImgStan = document.querySelector('.one img.stan');
 var square1ImgNetflix = document.querySelector('.one img.netflix');
-
-var square2Img = document.querySelector('.two');
-var square3Img = document.querySelector('.three');
-var square4Img = document.querySelector('.four');
-var square5Img = document.querySelector('.five');
-var square6Img = document.querySelector('.six');
-var square7Img = document.querySelector('.seven');
-var square8Img = document.querySelector('.eight');
-var square9Img = document.querySelector('.nine');
-
-
+var square2ImgStan = document.querySelector('.two img.stan');
+var square2ImgNetflix = document.querySelector('.two img.netflix');
+var square3ImgStan = document.querySelector('.three img.stan');
+var square3ImgNetflix = document.querySelector('.three img.netflix');
+var square4ImgStan = document.querySelector('.four img.stan');
+var square4ImgNetflix = document.querySelector('.four img.netflix');
+var square5ImgStan = document.querySelector('.five img.stan');
+var square5ImgNetflix = document.querySelector('.five img.netflix');
+var square6ImgStan = document.querySelector('.six img.stan');
+var square6ImgNetflix = document.querySelector('.six img.netflix');
+var square7ImgStan = document.querySelector('.seven img.stan');
+var square7ImgNetflix = document.querySelector('.seven img.netflix');
+var square8ImgStan = document.querySelector('.eight img.stan');
+var square8ImgNetflix = document.querySelector('.eight img.netflix');
+var square9ImgStan = document.querySelector('.nine img.stan');
+var square9ImgNetflix = document.querySelector('.nine img.netflix');
 
 //to randomly allocate one of the players 'your turn' - first turn
 function allocateFirstPlayer () {
@@ -49,7 +54,38 @@ function selectedNetflix () {
 		square1.classList.add('chosen-netflix');
 		square1ImgNetflix.classList.remove('hidden');
 	}
-
+	if (event.target === square2) {
+		square2.classList.add('chosen-netflix');
+		square2ImgNetflix.classList.remove('hidden');
+	}
+	if (event.target === square3) {
+		square3.classList.add('chosen-netflix');
+		square3ImgNetflix.classList.remove('hidden');
+	}
+	if (event.target === square4) {
+		square4.classList.add('chosen-netflix');
+		square4ImgNetflix.classList.remove('hidden');
+	}
+	if (event.target === square5) {
+		square5.classList.add('chosen-netflix');
+		square5ImgNetflix.classList.remove('hidden');
+	}
+	if (event.target === square6) {
+		square6.classList.add('chosen-netflix');
+		square6ImgNetflix.classList.remove('hidden');
+	}
+	if (event.target === square7) {
+		square7.classList.add('chosen-netflix');
+		square7ImgNetflix.classList.remove('hidden');
+	}
+	if (event.target === square8) {
+		square8.classList.add('chosen-netflix');
+		square8ImgNetflix.classList.remove('hidden');
+	}
+	if (event.target === square9) {
+		square9.classList.add('chosen-netflix');
+		square9ImgNetflix.classList.remove('hidden');
+	}
 };
 
 //to show the Stan img on a chosen square
@@ -58,70 +94,103 @@ function selectedStan () {
 		square1.classList.add('chosen-stan');
 		square1ImgStan.classList.remove('hidden');
 	} 
+	if (event.target === square2) {
+		square2.classList.add('chosen-stan');
+		square2ImgStan.classList.remove('hidden');
+	} 
+	if (event.target === square3) {
+		square3.classList.add('chosen-stan');
+		square3ImgStan.classList.remove('hidden');
+	} 
+	if (event.target === square4) {
+		square4.classList.add('chosen-stan');
+		square4ImgStan.classList.remove('hidden');
+	} 
+	if (event.target === square5) {
+		square5.classList.add('chosen-stan');
+		square5ImgStan.classList.remove('hidden');
+	} 
+	if (event.target === square6) {
+		square6.classList.add('chosen-stan');
+		square6ImgStan.classList.remove('hidden');
+	} 
+	if (event.target === square7) {
+		square7.classList.add('chosen-stan');
+		square7ImgStan.classList.remove('hidden');
+	} 
+	if (event.target === square8) {
+		square8.classList.add('chosen-stan');
+		square8ImgStan.classList.remove('hidden');
+	} 
+	if (event.target === square9) {
+		square9.classList.add('chosen-stan');
+		square9ImgStan.classList.remove('hidden');
+	} 
 };
 
 //to claim square for player, then changing the turn to other player
 function chooseSquare () {
 	if (playerNetflix.classList.contains('your-turn') === true) {
 		selectedNetflix();
+		checkForWin();
 		playerStan.classList.add('your-turn');	
 		playerNetflix.classList.remove('your-turn');
 	} else {
 		selectedStan();
+		checkForWin();
 		playerNetflix.classList.add('your-turn');	
-		playerStan.classList.remove('your-turn');	
+		playerStan.classList.remove('your-turn');
 	}
-	checkForWin();
 };
 
 //checking for one of eight win conditions, per player
 function checkForWin () {
-	if (square1.classList.contains('chosen-netflix') && square2.classList.contains('chosenp1') && square3.classList.contains('chosenp1') === true) {
+	if (square1.classList.contains('chosen-netflix') && square2.classList.contains('chosen-netflix') && square3.classList.contains('chosen-netflix') === true) {
 		winScreenNetflix.classList.remove('hidden');
 		restart.classList.remove('hidden');
-	} else if (square1.classList.contains('chosen-stan') && square2.classList.contains('chosenp2') && square3.classList.contains('chosenp2') === true) {
+	} else if (square1.classList.contains('chosen-stan') && square2.classList.contains('chosen-stan') && square3.classList.contains('chosen-stan') === true) {
 		winScreenStan.classList.remove('hidden');
 		restart.classList.remove('hidden');
-	} else if (square4.classList.contains('chosen-netflix') && square5.classList.contains('chosenp1') && square6.classList.contains('chosenp1') === true) {
+	} else if (square4.classList.contains('chosen-netflix') && square5.classList.contains('chosen-netflix') && square6.classList.contains('chosen-netflix') === true) {
 		winScreenNetflix.classList.remove('hidden');
 		restart.classList.remove('hidden');
-	} else if (square4.classList.contains('chosen-stan') && square5.classList.contains('chosenp2') && square6.classList.contains('chosenp2') === true) {
+	} else if (square4.classList.contains('chosen-stan') && square5.classList.contains('chosen-stan') && square6.classList.contains('chosen-stan') === true) {
 		winScreenStan.classList.remove('hidden');
 		restart.classList.remove('hidden');
-	} else if (square7.classList.contains('chosen-netflix') && square8.classList.contains('chosenp1') && square9.classList.contains('chosenp1') === true) {
+	} else if (square7.classList.contains('chosen-netflix') && square8.classList.contains('chosen-netflix') && square9.classList.contains('chosen-netflix') === true) {
 		winScreenNetflix.classList.remove('hidden');
 		restart.classList.remove('hidden');
-	} else if (square7.classList.contains('chosen-stan') && square8.classList.contains('chosenp2') && square9.classList.contains('chosenp2') === true) {
+	} else if (square7.classList.contains('chosen-stan') && square8.classList.contains('chosen-stan') && square9.classList.contains('chosen-stan') === true) {
 		winScreenStan.classList.remove('hidden');
 		restart.classList.remove('hidden');
-	} else if (square1.classList.contains('chosen-netflix') && square4.classList.contains('chosenp1') && square7.classList.contains('chosenp1') === true) {
+	} else if (square1.classList.contains('chosen-netflix') && square4.classList.contains('chosen-netflix') && square7.classList.contains('chosen-netflix') === true) {
 		winScreenNetflix.classList.remove('hidden');
 		restart.classList.remove('hidden');
-	} else if (square1.classList.contains('chosen-stan') && square4.classList.contains('chosenp2') && square7.classList.contains('chosenp2') === true) {
+	} else if (square1.classList.contains('chosen-stan') && square4.classList.contains('chosen-stan') && square7.classList.contains('chosen-stan') === true) {
 		winScreenStan.classList.remove('hidden');
 		restart.classList.remove('hidden');
-	} else if (square3.classList.contains('chosen-netflix') && square6.classList.contains('chosenp1') && square9.classList.contains('chosenp1') === true) {
+	} else if (square3.classList.contains('chosen-netflix') && square6.classList.contains('chosen-netflix') && square9.classList.contains('chosen-netflix') === true) {
 		winScreenNetflix.classList.remove('hidden');
 		restart.classList.remove('hidden');
-	} else if (square3.classList.contains('chosen-stan') && square6.classList.contains('chosenp2') && square9.classList.contains('chosenp2') === true) {
+	} else if (square3.classList.contains('chosen-stan') && square6.classList.contains('chosen-stan') && square9.classList.contains('chosen-stan') === true) {
 		winScreenStan.classList.remove('hidden');
 		restart.classList.remove('hidden');
-	} else if (square1.classList.contains('chosen-netflix') && square5.classList.contains('chosenp1') && square9.classList.contains('chosenp1') === true) {
+	} else if (square1.classList.contains('chosen-netflix') && square5.classList.contains('chosen-netflix') && square9.classList.contains('chosen-netflix') === true) {
 		winScreenNetflix.classList.remove('hidden');
 		restart.classList.remove('hidden');
-	} else if (square1.classList.contains('chosen-stan') && square5.classList.contains('chosenp2') && square9.classList.contains('chosenp2') === true) {
+	} else if (square1.classList.contains('chosen-stan') && square5.classList.contains('chosen-stan') && square9.classList.contains('chosen-stan') === true) {
 		winScreenStan.classList.remove('hidden');
 		restart.classList.remove('hidden');
-	} else if (square3.classList.contains('chosen-netflix') && square5.classList.contains('chosenp1') && square7.classList.contains('chosenp1') === true) {
+	} else if (square3.classList.contains('chosen-netflix') && square5.classList.contains('chosen-netflix') && square7.classList.contains('chosen-netflix') === true) {
 		winScreenNetflix.classList.remove('hidden');
 		restart.classList.remove('hidden');
-	} else if (square3.classList.contains('chosen-stan') && square5.classList.contains('chosenp2') && square7.classList.contains('chosenp2') === true) {
+	} else if (square3.classList.contains('chosen-stan') && square5.classList.contains('chosen-stan') && square7.classList.contains('chosen-stan') === true) {
 		winScreenStan.classList.remove('hidden');
 		restart.classList.remove('hidden');
-	} else if (square2.classList.contains('chosen-netflix') && square5.classList.contains('chosenp1') && square8.classList.contains('chosenp1') === true) {
+	} else if (square2.classList.contains('chosen-netflix') && square5.classList.contains('chosen-netflix') && square8.classList.contains('chosen-netflix') === true) {
 		winScreenNetflix.classList.remove('hidden');
 		restart.classList.remove('hidden');
-	} else if (square2.classList.contains('chosen-stan') && square5.classList.contains('chosenp2') && square8.classList.contains('chosenp2') === true) {
+	} else if (square2.classList.contains('chosen-stan') && square5.classList.contains('chosen-stan') && square8.classList.contains('chosen-stan') === true) {
 		winScreenStan.classList.remove('hidden');
 		restart.classList.remove('hidden');
 	}
